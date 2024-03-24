0efd2ab5560e9d27558bed6dff2bf5b7ab7d9f4e
Ljiljana Skavic <ljiljana.skavic@hotmail.com>
2024-03-24 18:15:35 +0100

    Add custom loader

--- src/app/components/exercise-list/exercise-list.component.ts
+++ src/app/components/exercise-list/exercise-list.component.ts
@@ -4,39 +4,40 @@ import { Subscription } from "rxjs";
 import { Exercise } from "../../models/Exercise";
 
 @Component({
-  selector: 'app-exercise-list',
-  templateUrl: './exercise-list.component.html',
-  styleUrls: ['./exercise-list.component.scss']
+    selector: 'app-exercise-list',
+    templateUrl: './exercise-list.component.html',
+    styleUrls: ['./exercise-list.component.scss']
 })
 export class ExerciseListComponent implements OnInit, OnDestroy {
-  subs = new Subscription();
-  exercises: Exercise[] = [];
-  pagedExercises: Exercise[] = [];
-  isLoading = false;
-  pageSizeOptions: any;
-  pageSize = 10;
+    subs = new Subscription();
+    exercises: Exercise[] = [];
+    pagedExercises: Exercise[] = [];
+    isLoading = true;
+    pageSizeOptions: any;
+    pageSize = 10;
 
-  constructor(private _exercisesService: ExercisesService,) {
-  }
+    constructor(private _exercisesService: ExercisesService,) {
+    }
 
-  ngOnInit(): void {
-    this.subs.add(this._exercisesService.getExercises().subscribe(res => {
-      this.exercises = res;
-      this.pagedExercises = res;
-    }));
-  }
+    ngOnInit(): void {
+        this.subs.add(this._exercisesService.getExercises().subscribe(res => {
+            this.exercises = res;
+            this.pagedExercises = res;
+            this.isLoading = false;
+        }));
+    }
 
-  onPageChange(event: { pageIndex: number; }): void {
-    this.displayPage(event.pageIndex);
-  }
+    onPageChange(event: { pageIndex: number; }): void {
+        this.displayPage(event.pageIndex);
+    }
 
-  displayPage(pageIndex: number): void {
-    const startIndex = pageIndex * this.pageSize;
-    const endIndex = startIndex + this.pageSize;
-    this.pagedExercises = this.exercises.slice(startIndex, endIndex);
-  }
+    displayPage(pageIndex: number): void {
+        const startIndex = pageIndex * this.pageSize;
+        const endIndex = startIndex + this.pageSize;
+        this.pagedExercises = this.exercises.slice(startIndex, endIndex);
+    }
 
-  ngOnDestroy(): void {
-    this.subs.unsubscribe();
-  }
+    ngOnDestroy(): void {
+        this.subs.unsubscribe();
+    }
 }
