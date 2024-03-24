9af464857f6756d5a031ce75312f9df277cd648e
Ljiljana Skavic <ljiljana.skavic@hotmail.com>
2024-03-24 12:01:07 +0100

    Fix deleting purchases

--- src/app/components/fitness-purchases-list/fitness-program-purchase-card/fitness-program-purchase-card.component.ts
+++ src/app/components/fitness-purchases-list/fitness-program-purchase-card/fitness-program-purchase-card.component.ts
@@ -6,36 +6,35 @@ import { MatDialog } from "@angular/material/dialog";
 import { FitnessProgramPurchaseService } from "../../../services/fitness-program-purchase.service";
 
 @Component({
-  selector: 'app-fitness-program-purchase-card',
-  templateUrl: './fitness-program-purchase-card.component.html',
-  styleUrls: ['./fitness-program-purchase-card.component.scss']
+    selector: 'app-fitness-program-purchase-card',
+    templateUrl: './fitness-program-purchase-card.component.html',
+    styleUrls: ['./fitness-program-purchase-card.component.scss']
 })
 export class FitnessProgramPurchaseCardComponent implements OnDestroy {
 
-  @Input() purchase: FitnessProgramPurchase;
-  @Output() purchaseDeletedEmitter = new EventEmitter<number>();
-  subscription = new Subscription();
+    @Input() purchase: FitnessProgramPurchase;
+    @Output() purchaseDeletedEmitter = new EventEmitter<number>();
+    subscription = new Subscription();
 
-  constructor(private _fitnessProgramPurchaseService: FitnessProgramPurchaseService,
-              public dialog: MatDialog) {
+    constructor(private _fitnessProgramPurchaseService: FitnessProgramPurchaseService,
+                public dialog: MatDialog) {
 
-  }
+    }
 
+    onDeletePurchase(id: number) {
+        this.dialog.open(ConfirmationModalComponent, {
+            data: {
+                title: "Delete purchase history",
+                text: "Are you sure that you want to delete this purchase?"
+            }
+        }).afterClosed().pipe(switchMap(result => {
+            return result ? this._fitnessProgramPurchaseService.deleteById(id) : EMPTY
+        })).subscribe(res => {
+            this.purchaseDeletedEmitter.emit(id);
+        })
+    }
 
-  onDeletePurchase(id: number) {
-    this.dialog.open(ConfirmationModalComponent, {
-      data: {
-        title: "Delete purchase history",
-        text: "Are you sure that you want to delete this purchase?"
-      }
-    }).afterClosed().pipe(switchMap(result => {
-      return result ? this._fitnessProgramPurchaseService.deleteById(id) : EMPTY
-    })).subscribe(res => {
-      this.purchaseDeletedEmitter.emit(id);
-    })
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
 }
