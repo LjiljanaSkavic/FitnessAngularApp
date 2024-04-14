b18e63eb61ae7a416170549b418bd7e06132ab8d
Ljiljana Skavic <ljiljana.skavic@hotmail.com>
2024-04-14 09:43:25 +0200

    Implement chat

--- src/app/components/chat/chat-message/chat-message.component.ts
+++ src/app/components/chat/chat-message/chat-message.component.ts
@@ -1,15 +1,56 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { ChatMessage } from "../../../models/dto/ChatMessage";
+import { FormControl, FormGroup } from "@angular/forms";
+import { UserStoreService } from "../../../services/user-store.service";
+import { FileService } from "../../../services/file.service";
+import { AppUser, AppUserShort } from "../../../models/AppUser";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-chat-message',
-  templateUrl: './chat-message.component.html',
-  styleUrls: ['./chat-message.component.scss']
+    selector: 'app-chat-message',
+    templateUrl: './chat-message.component.html',
+    styleUrls: ['./chat-message.component.scss']
 })
-export class ChatMessageComponent implements OnInit {
+export class ChatMessageComponent implements OnInit, OnDestroy {
 
-  @Input() message: ChatMessage = {} as ChatMessage;
+    @Input() message: ChatMessage = {} as ChatMessage;
+    @Input() selectedUser: AppUserShort = {} as AppUserShort;
+    imageURL = '';
+    messageForm: FormGroup;
+    user: AppUser;
+    isMyMessage = false;
 
-  ngOnInit(): void {
-  }
+    subscriptions = new Subscription();
+
+    constructor(private _userStoreService: UserStoreService,
+                private _fileService: FileService) {
+    }
+
+
+    ngOnInit(): void {
+        if (this._userStoreService.getIsLoggedIn()) {
+            this.user = this._userStoreService.getLoggedInUser();
+            this.isMyMessage = this.user.id === this.message.appUserCreator;
+        }
+
+        if (!this.imageURL) {
+            const userId = this.message.appUserCreator === this.user.id ? this.user.id : this.selectedUser.image.id;
+            this.subscriptions.add(
+                this._fileService.getFileById(userId).subscribe(imageBlob => {
+                    this.imageURL = URL.createObjectURL(imageBlob);
+                })
+            );
+        }
+
+        this.messageForm = new FormGroup({
+            message: new FormControl(this.message.text),
+        })
+
+        this.messageForm.get('message').disable();
+    }
+
+
+    ngOnDestroy(): void {
+        this.subscriptions.unsubscribe();
+    }
 }
--- src/app/components/chat/messages/messages.component.ts
+++ src/app/components/chat/messages/messages.component.ts
@@ -4,69 +4,68 @@ import { UserStoreService } from "../../../services/user-store.service";
 import { ChatService } from "../../../services/chat.service";
 import { Subscription, switchMap } from "rxjs";
 import { ChatMessage } from "../../../models/dto/ChatMessage";
-import { FormControl, FormGroup } from "@angular/forms";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-messages',
-  templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.scss']
+    selector: 'app-messages',
+    templateUrl: './messages.component.html',
+    styleUrls: ['./messages.component.scss']
 })
 export class MessagesComponent implements OnInit, OnChanges {
 
-  @Input() selectedUser: AppUserShort = {} as AppUserShort;
-  messages: ChatMessage[] = [];
-  chatId: number;
+    @Input() selectedUser: AppUserShort = {} as AppUserShort;
+    messages: ChatMessage[] = [];
+    chatId: number;
+    userId: number;
+    sendMessageForm: FormGroup;
 
-  userId: number;
+    subscriptions = new Subscription();
 
-  subscriptions = new Subscription();
-  sendMessageForm: FormGroup;
-
-  constructor(private _userStoreService: UserStoreService, private _chatService: ChatService) {
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (!changes['selectedUser'].firstChange) {
-      this.subscriptions.add(
-        this._chatService.getChat(this.userId, changes['selectedUser'].currentValue.id)
-          .pipe(
-            switchMap(res => {
-              this.chatId = res;
-              return this._chatService.getMessages(this.chatId);
-            })
-          )
-          .subscribe(messages => {
-            this.messages = messages;
-          })
-      );
-    }
-  }
-
-  ngOnInit(): void {
-    if (this._userStoreService.getIsLoggedIn()) {
-      this.userId = this._userStoreService.getLoggedInUser().id;
+    constructor(private _userStoreService: UserStoreService,
+                private _chatService: ChatService) {
     }
 
-    this.sendMessageForm = new FormGroup({
-      messageText: new FormControl('')
-    })
-
-  }
-
-  onSendMessageClick(): void {
-    const newChatMessage: ChatMessage = {
-      appUserCreator: this.userId,
-      chatId: this.chatId,
-      dateTime: new Date(),
-      id: 0,
-      text: this.sendMessageForm.get('messageText').value
+    ngOnChanges(changes: SimpleChanges): void {
+        if (!changes['selectedUser'].firstChange) {
+            this.subscriptions.add(
+                this._chatService.getChat(this.userId, changes['selectedUser'].currentValue.id)
+                    .pipe(
+                        switchMap(res => {
+                            this.chatId = res;
+                            return this._chatService.getMessages(this.chatId);
+                        })
+                    )
+                    .subscribe(messages => {
+                        this.messages = messages;
+                    })
+            );
+        }
     }
 
-    this._chatService.sendMessage(this.chatId, newChatMessage).subscribe(res => {
-      console.log(res);
-      this.sendMessageForm.get('messageText').setValue('');
-      this.messages.push(res);
-    })
-  }
+    ngOnInit(): void {
+        if (this._userStoreService.getIsLoggedIn()) {
+            this.userId = this._userStoreService.getLoggedInUser().id;
+        }
+
+        this.sendMessageForm = new FormGroup({
+            messageText: new FormControl('', Validators.required)
+        })
+
+    }
+
+    onSendMessageClick(): void {
+        const newChatMessage: ChatMessage = {
+            appUserCreator: this.userId,
+            chatId: this.chatId,
+            dateTime: new Date(),
+            id: 0,
+            text: this.sendMessageForm.get('messageText').value
+        }
+
+        this._chatService.sendMessage(this.chatId, newChatMessage).subscribe(res => {
+            this.sendMessageForm.get('messageText').setValue('');
+            this.messages.push(res);
+        })
+    }
 
 }
