488975cfdf677254b93f0933eb84bdf37f9407de
Ljiljana Skavic <ljiljana.skavic@hotmail.com>
2024-05-02 13:18:30 +0200

    Implement chat

--- src/app/components/chat/user-list/user-list.component.ts
+++ src/app/components/chat/user-list/user-list.component.ts
@@ -1,36 +1,60 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { AppUserShort } from "../../../models/AppUser";
+import { AppUser, AppUserShort } from "../../../models/AppUser";
 import { Subscription } from "rxjs";
 import { UserService } from "../../../services/user.service";
 import { UserStoreService } from "../../../services/user-store.service";
+import { FileService } from "../../../services/file.service";
 
 @Component({
-    selector: 'app-user-list',
-    templateUrl: './user-list.component.html',
-    styleUrls: ['./user-list.component.scss']
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent {
 
-    @Output() selectedUserEmitter = new EventEmitter<AppUserShort>();
-    userId: number;
-    activatedUsers: AppUserShort[] = [];
-    subscriptions = new Subscription();
+  @Output() selectedUserEmitter = new EventEmitter<AppUserShort>();
+  user: AppUser;
+  activatedUsers: AppUserShort[] = [];
+  subscriptions = new Subscription();
+  userImageMap: Map<number, string> = new Map();
+  selectedUserId: number;
 
-    constructor(private _userService: UserService,
-                private _userStoreService: UserStoreService) {
-    }
+  loggedUserUrlImage: string = '';
 
-    ngOnInit() {
-        if (this._userStoreService.getIsLoggedIn()) {
-            this.userId = this._userStoreService.getLoggedInUser().id;
-        }
-        this.subscriptions.add(this._userService.getActiveUsers().subscribe(res => {
-            this.activatedUsers = res.filter(user => user.id !== this.userId);
-            console.log(res);
-        }));
-    }
+  constructor(private _userService: UserService,
+              private _userStoreService: UserStoreService,
+              private _fileService: FileService) {
+  }
 
-    selectUser(user: AppUserShort): void {
-        this.selectedUserEmitter.emit(user);
+  ngOnInit() {
+    if (this._userStoreService.getIsLoggedIn()) {
+      this.user = this._userStoreService.getLoggedInUser();
+      this.subscriptions.add(
+        this._fileService.getFileById(this.user.image.id).subscribe(imageBlob => {
+          const imageUrl = URL.createObjectURL(imageBlob);
+          this.userImageMap.set(this.user.id, imageUrl);
+          return imageUrl;
+        })
+      );
     }
+    this.subscriptions.add(this._userService.getActiveUsers().subscribe(res => {
+      this.activatedUsers = res.filter(user => user.id !== this.user.id);
+    }));
+  }
+
+  getUrl(user: AppUserShort | AppUser): string {
+    this.subscriptions.add(
+      this._fileService.getFileById(user.image.id).subscribe(imageBlob => {
+        const imageUrl = URL.createObjectURL(imageBlob);
+        this.userImageMap.set(user.id, imageUrl);
+        return imageUrl;
+      })
+    );
+    return '';
+  }
+
+  selectUser(user: AppUserShort): void {
+    this.selectedUserEmitter.emit(user);
+    this.selectedUserId = user.id;
+  }
 }
