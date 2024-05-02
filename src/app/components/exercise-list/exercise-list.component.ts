a5f5cf02ec9e709acdc9b78749f27c056b285760
Ljiljana Skavic <ljiljana.skavic@hotmail.com>
2024-05-02 17:33:06 +0200

    Add paginator to exercise list

--- src/app/components/exercise-list/exercise-list.component.ts
+++ src/app/components/exercise-list/exercise-list.component.ts
@@ -1,43 +1,46 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ExercisesService } from "../../services/exercises.service";
 import { Subscription } from "rxjs";
 import { Exercise } from "../../models/Exercise";
+import { MatPaginator } from "@angular/material/paginator";
 
 @Component({
-    selector: 'app-exercise-list',
-    templateUrl: './exercise-list.component.html',
-    styleUrls: ['./exercise-list.component.scss']
+  selector: 'app-exercise-list',
+  templateUrl: './exercise-list.component.html',
+  styleUrls: ['./exercise-list.component.scss']
 })
 export class ExerciseListComponent implements OnInit, OnDestroy {
-    subs = new Subscription();
-    exercises: Exercise[] = [];
-    pagedExercises: Exercise[] = [];
-    isLoading = true;
-    pageSizeOptions: any;
-    pageSize = 10;
+  subs = new Subscription();
+  exercises: Exercise[] = [];
+  pagedExercises: Exercise[] = [];
+  isLoading = true;
+  pageSizeOptions: any;
+  pageSize = 5;
 
-    constructor(private _exercisesService: ExercisesService,) {
-    }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
-    ngOnInit(): void {
-        this.subs.add(this._exercisesService.getExercises().subscribe(res => {
-            this.exercises = res;
-            this.pagedExercises = res;
-            this.isLoading = false;
-        }));
-    }
+  constructor(private _exercisesService: ExercisesService,) {
+  }
 
-    onPageChange(event: { pageIndex: number; }): void {
-        this.displayPage(event.pageIndex);
-    }
+  ngOnInit(): void {
+    this.subs.add(this._exercisesService.getExercises().subscribe(res => {
+      this.exercises = res;
+      this.displayPage(0, 0)
+      this.isLoading = false;
+    }));
+  }
 
-    displayPage(pageIndex: number): void {
-        const startIndex = pageIndex * this.pageSize;
-        const endIndex = startIndex + this.pageSize;
-        this.pagedExercises = this.exercises.slice(startIndex, endIndex);
-    }
+  onPageChange(event: any): void {
+    this.displayPage(event.pageIndex, event.previousPageIndex);
+  }
 
-    ngOnDestroy(): void {
-        this.subs.unsubscribe();
-    }
+  displayPage(currentPageIndex: number, previousPageIndex: number): void {
+    const startIndex = currentPageIndex * this.pageSize;
+    const endIndex = currentPageIndex >= previousPageIndex ? startIndex + this.pageSize : startIndex - this.pageSize;
+    this.pagedExercises = this.exercises.slice(startIndex, endIndex);
+  }
+
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
+  }
 }
