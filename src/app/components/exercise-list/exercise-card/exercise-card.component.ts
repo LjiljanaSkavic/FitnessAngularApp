1f2f8c7fb929a68ea789f9fe8bd7dbc115be2b4a
Ljiljana Skavic <ljiljana.skavic@hotmail.com>
2024-05-27 19:03:32 +0200

    Fix style on fitness cards

--- src/app/components/exercise-list/exercise-card/exercise-card.component.ts
+++ src/app/components/exercise-list/exercise-card/exercise-card.component.ts
@@ -2,16 +2,14 @@ import { Component, Input } from '@angular/core';
 import { Exercise } from "../../../models/Exercise";
 
 @Component({
-    selector: 'app-exercise-card',
-    templateUrl: './exercise-card.component.html',
-    styleUrls: ['./exercise-card.component.scss']
+  selector: 'app-exercise-card',
+  templateUrl: './exercise-card.component.html',
+  styleUrls: ['./exercise-card.component.scss']
 })
 export class ExerciseCardComponent {
 
-    @Input() exercise: Exercise = {} as Exercise;
+  @Input() exercise: Exercise = {} as Exercise;
 
-    constructor() {
-
-    }
-
+  constructor() {
+  }
 }
